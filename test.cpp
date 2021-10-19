db0ff0ca69437363ee5b560b6f95057c12997cac
Samito-uc3m <100429112@alumnos.uc3m.es>
2021-10-19 23:10:43 +0200

    Co-authored-by: Juan Franco Labarra <JuanFraLab@users.noreply.github.com> Co-authored-by: alberto-mate <alberto-mate@users.noreply.github.com> Co-authored-by: Hugo García <xHugo21@users.noreply.github.com>

--- test.cpp
+++ test.cpp
@@ -13,9 +13,9 @@
 using namespace std;
 
 /* CONSTANTES */
-const double GRAVITY_CONST = 6.674 * pow(10, -11); // Constante gravedad universal
-const double M = pow(10, 21);                      // Media (distribución normal)
-const double SDM = pow(10, 15);                    // Desviación (distribución normal)
+const double GRAVITY_CONST = 6.674 * 10E-11; // Constante gravedad universal
+const double M = 1E21;                     // Media (distribución normal)
+const double SDM = 1E15;                    // Desviación (distribución normal)
 
 /* ESTRUCTURAS */
 /* Estructura objeto */
@@ -42,9 +42,9 @@ struct vector_elem
 void vector_gravitational_force(object object_1, object object_2, double* forces);
 void calc_gravitational(int num_objects, int index_1, vector<object> &objects, double* forces);
 void vector_acceleration(object object_1, double* forces, vector_elem* acceleration);
-void vector_speed(object *object_1, vector_elem *acceleration, float time_step);
-void vector_position(object *object_1, float time_step);
-void check_border(object *object_1, float size_enclosure);
+void vector_speed(object *object_1, vector_elem *acceleration, double time_step);
+void vector_position(object *object_1, double time_step);
+void check_border(object *object_1, double size_enclosure);
 bool check_collision(object object_1, object object_2);
 
 /* MAIN */
@@ -70,8 +70,8 @@ int main(int argc, char const *argv[])
     int num_objects = atoi(argv[1]);      // Número de objetos a simular (>0 entero)
     int num_iterations = atoi(argv[2]);   // Número de iteraciones a simular (>0 entero)
     int random_seed = atoi(argv[3]);      // Semilla para distribuciones aleatorias
-    float size_enclosure = atof(argv[4]); // Tamaño del recinto (>0 real)
-    float time_step = atof(argv[5]);      // Incremento de tiempo en cada iteración (>0 real)
+    double size_enclosure = stod(argv[4]); // Tamaño del recinto (>0 real)
+    double time_step = stod(argv[5]);      // Incremento de tiempo en cada iteración (>0 real)
 
     /* Impresión por pantalla de las variables */
     cout << "num_objects: " << num_objects << "\n";
@@ -81,9 +81,9 @@ int main(int argc, char const *argv[])
     cout << "time_step: " << time_step << "\n";
 
     /* Coordenadas aleatorias */
-    mt19937_64 gen(random_seed);
+    mt19937_64 gen64(random_seed);
     uniform_real_distribution<> position_dist(0.0, size_enclosure);
-    normal_distribution<> disMas(10e21, 10e15);
+    normal_distribution<> mass_dist(M, SDM);
 
     /* AOS - Array of Structs */
     //object *objects = (object*) malloc(sizeof(object)*num_objects);
@@ -97,13 +97,14 @@ int main(int argc, char const *argv[])
     /* Creación de objetos */
     for (int i = 0; i < num_objects; i++)
     {
-        objects[i].pos_x = position_dist(gen); // Posicion x, y, z
-        objects[i].pos_y = position_dist(gen);
-        objects[i].pos_z = position_dist(gen);
+        objects[i].pos_x = position_dist(gen64); // Posicion x, y, z
+        objects[i].pos_y = position_dist(gen64);
+        objects[i].pos_z = position_dist(gen64);
         objects[i].speed_x = 0; // Velocidad x, y, z
         objects[i].speed_y = 0;
         objects[i].speed_z = 0;
-        objects[i].mass = disMas(gen); // Masa
+        objects[i].mass = mass_dist(gen64); // Masa
+        cout<<(objects[i].mass)<<"\n";
 
         // Ponemos la precisión a 3 decimales. Imprimimos el objeto
         file_init << fixed << setprecision(3) << objects[i].pos_x << " " << objects[i].pos_y << " " << objects[i].pos_z << " " << objects[i].speed_x << " " << objects[i].speed_y << " " << objects[i].speed_z << " " << objects[i].mass << endl;
@@ -120,11 +121,11 @@ int main(int argc, char const *argv[])
         /* Bucle para obtener nuevas propiedades de los objetos en la iteración. Comprueba también que no se haya pasado de los límites.*/
         for (int i = 0; i < num_objects; i++){
             if (objects[i].mass!=0.0){ // Solo entrarán en el condicional objetos que no se han eliminado
-
                 // Cálculo de la fuerza gravitatoria
-                double forces[3] = {0.0,0.0,0.0};
+                double forces[3] = {0,0,0};
                 calc_gravitational(num_objects, i, objects, forces);
-                //cout<< "Obj: " << i<<" posx: "<<objects[i].pos_x<<" posy: "<< objects[i].pos_y<<" posz: "<<objects[i].pos_z<<" speedx: "<<objects[i].speed_x<<" speedy: "<< objects[i].speed_y<<" speedz: "<<objects[i].speed_z<<"\n";
+                //cout<< "Obj: " << i<<" posx: "<<objects[i].pos_x<<" posy: "<< objects[i].pos_y<<" posz: "<<objects[i].pos_z<<" speedx: "<<objects[i].speed_x<<" speedy: "<< objects[i].speed_y<<" speedz: "<<objects[i].speed_z<<" mass: "<<objects[i].mass<<"\n";
+                cout<<"Forces 1 "<<" ax: "<<forces[0]<<" ay: "<<forces[1]<<" az: "<<forces[2]<<"\n";
                 // Cálculo del vector aceleración
                 vector_elem *acceleration = (vector_elem*) malloc(sizeof(vector_elem));
                 vector_acceleration(objects[i], forces, acceleration);
@@ -135,39 +136,47 @@ int main(int argc, char const *argv[])
                 // Cálculo del vector posiciones
                 vector_position(&objects[i], time_step);
                 //cout<< "Obj: " << i<<" posx: "<<objects[i].pos_x<<" posy: "<< objects[i].pos_y<<" posz: "<<objects[i].pos_z<<" speedx: "<<objects[i].speed_x<<" speedy: "<< objects[i].speed_y<<" speedz: "<<objects[i].speed_z<<"\n";
-                
+                // Comprobar bordes
                 check_border(&objects[i], size_enclosure);
-                
                 cout<< "Obj: " << i<<" posx: "<<objects[i].pos_x<<" posy: "<< objects[i].pos_y<<" posz: "<<objects[i].pos_z<<" speedx: "<<objects[i].speed_x<<" speedy: "<< objects[i].speed_y<<" speedz: "<<objects[i].speed_z<<"\n";
             }
         } 
         //cout<<"Nuevas posiciones calculadas \n";
-
+        int deletes[num_objects];
         int num_objects_after_delete = num_objects;
         /* Bucle anidado para comprobar colisiones entre objetos */
-        for (int i = 0; i < objects.size(); i++){
-            for (int j = i+1; j < objects.size(); j++){ // TODO AÑADIR OPTI num_objetos -i-1 objects[j+i+1]
+        for (int i = 0; i < num_objects; i++){
+            for (int j = i+1; j < num_objects-i; j++){ // TODO AÑADIR OPTI num_objetos -i-1 objects[j+i+1]
                 // Se resta i y 1 para evitar comprobaciones dobles
                 // Comprobar colisiones
                 if (i!=j){ // Colision entre objetos diferentes que no hayan sido eliminados con anterioridad
                     if (check_collision(objects[i], objects[j])){ // Comprobar colisión
+                        if (deletes[j]!=1 && deletes[i]!=1){
+                            // Actualización de la masa y velocidades del primer objeto que colisiona generando uno nuevo
+                            objects[i].mass += objects[j].mass;
+                            objects[i].speed_x += objects[j].speed_x;
+                            objects[i].speed_y += objects[j].speed_y;
+                            objects[i].speed_z += objects[j].speed_z;
 
-                        // Actualización de la masa y velocidades del primer objeto que colisiona generando uno nuevo
-                        objects[i].mass += objects[j].mass;
-                        objects[i].speed_x += objects[j].speed_x;
-                        objects[i].speed_y += objects[j].speed_y;
-                        objects[i].speed_z += objects[j].speed_z;
-
-                        // Eliminación del segundo objeto que colisiona del sistema
-                        objects.erase(objects.begin()+j);
-                        j--;
+                            deletes[j]=1;
+                        }
                     }
                 }
             }
         }
-        
+        int contador = 0;
+        while (contador<num_objects){
+            if (deletes[contador]==1){
+                objects.erase(objects.begin()+contador);
+                num_objects -= 1;
+            }
+            contador++;
+        }
 
-        num_objects = objects.size();
+        cout<<"Fin iteración: "<<iteration<<" Num objetos:"<<num_objects<<"\n";
+
+        // Eliminación del segundo objeto que colisiona del sistema
+
         /* 
         // Actualizar array objects debido a colisiones
         
@@ -197,8 +206,6 @@ int main(int argc, char const *argv[])
         }
         num_objects = num_objects_after_delete;
         */
-
-        cout<<"Fin iteración: "<<iteration<<" Num objetos:"<<num_objects<<"\n";
     }
 
 }
@@ -207,17 +214,19 @@ int main(int argc, char const *argv[])
 /* Distancia euclídea entre dos objetos */
 double euclidean_norm(object object_1, object object_2)
 {   
+    //cout<<"Pos 2 "<<" px: "<<(object_1.pos_x)<<" "<< (object_2.pos_x)<<" "<<(object_1.pos_x - object_2.pos_x)<<" py: "<<(object_1.pos_y)<<" "<< (object_2.pos_y)<<" "<<(object_1.pos_y - object_2.pos_y)<<" pz: "<<(object_1.pos_z)<<" "<< (object_2.pos_z)<<" "<<(object_1.pos_z - object_2.pos_z)<<"\n";
     return std::sqrt((object_1.pos_x - object_2.pos_x)*(object_1.pos_x - object_2.pos_x) + (object_1.pos_y - object_2.pos_y)*(object_1.pos_y - object_2.pos_y) + (object_1.pos_z - object_2.pos_z)*(object_1.pos_z - object_2.pos_z));
 }
 
 /* Fuerza gravitatoria entre dos objetos */
 void vector_gravitational_force(object object_1, object object_2, double* forces){
+    //cout<<"Pos 1 "<<" px: "<<(object_1.pos_x)<<" "<< (object_2.pos_x)<<" "<<(object_1.pos_x - object_2.pos_x)<<" py: "<<(object_1.pos_y)<<" "<< (object_2.pos_y)<<" "<<(object_1.pos_y - object_2.pos_y)<<" pz: "<<(object_1.pos_z)<<" "<< (object_2.pos_z)<<" "<<(object_1.pos_z - object_2.pos_z)<<"\n";
+    //cout<<"Forces 1 "<<" ax: "<<forces[0]<<" ay: "<<forces[1]<<" az: "<<forces[2]<<"\n";
     double x = (euclidean_norm(object_1, object_2));
     if (x!=0){
-        double force_module = (GRAVITY_CONST * object_1.mass * object_2.mass) / (pow(x, 3));
-        forces[0] += force_module*(object_1.pos_x - object_2.pos_x);
-        forces[1] += force_module*(object_1.pos_y - object_2.pos_y);
-        forces[2] += force_module*(object_1.pos_z - object_2.pos_z);
+        forces[0] += (GRAVITY_CONST * object_1.mass * object_2.mass*(object_2.pos_x - object_1.pos_x)) / (x*x*x);
+        forces[1] += (GRAVITY_CONST * object_1.mass * object_2.mass*(object_2.pos_y - object_1.pos_y)) / (x*x*x);
+        forces[2] += (GRAVITY_CONST * object_1.mass * object_2.mass*(object_2.pos_z - object_1.pos_z)) / (x*x*x);
     }
 }
 
@@ -225,7 +234,9 @@ void vector_gravitational_force(object object_1, object object_2, double* forces
 void calc_gravitational(int num_objects, int i, std::vector<object> & objects, double* forces){
     for (int j=0; j<num_objects; j++){
         if (j!=i){
+            //cout<<"Forces 1 "<<j<<" ax: "<<forces[0]<<" ay: "<<forces[1]<<" az: "<<forces[2]<<"\n";
             vector_gravitational_force(objects[i], objects[j], forces);
+            //cout<<"Forces 2 "<<j<<" ax: "<<forces[0]<<" ay: "<<forces[1]<<" az: "<<forces[2]<<"\n";
         }
     }
 }
@@ -233,23 +244,33 @@ void calc_gravitational(int num_objects, int i, std::vector<object> & objects, d
 /* Vector aceleración */
 void vector_acceleration(object object_1, double* forces, vector_elem* acceleration)
 {
-    /* Cálculo del vector aceleración */
-    acceleration->x= forces[0]/object_1.mass;
-    acceleration->y= forces[1]/object_1.mass;
-    acceleration->z= forces[2]/object_1.mass;
+    if (object_1.mass!=0){
+        //cout<<"ax: "<<forces[0]<<" ay: "<<forces[1]<<" az: "<<forces[2]<<"\n";
+        /* Cálculo del vector aceleración */
+        acceleration->x= forces[0]/object_1.mass;
+        acceleration->y= forces[1]/object_1.mass;
+        acceleration->z= forces[2]/object_1.mass;
+        //cout<<"ax: "<<forces[0]<<" ay: "<<forces[1]<<" az: "<<forces[2]<<"\n";
+    }else{
+        acceleration->x= 0;
+        acceleration->y= 0;
+        acceleration->z= 0;
+    }
 }
 
 /* Vector velocidad */
-void vector_speed(object *object_1, vector_elem *acceleration, float time_step)
+void vector_speed(object *object_1, vector_elem *acceleration, double time_step)
 {      
+    //cout<<" posx: "<<object_1->pos_x<<" posy: "<< object_1->pos_y<<" posz: "<<object_1->pos_z<<" speedx: "<<object_1->speed_x<<" speedy: "<< object_1->speed_y<<" speedz: "<<object_1->speed_z<<"\n";
     /* Cálculo del vector velocidad */
     object_1->speed_x = object_1->speed_x + (acceleration->x * time_step);
     object_1->speed_y = object_1->speed_y + (acceleration->y * time_step);
     object_1->speed_z = object_1->speed_z + (acceleration->z * time_step);
+    //cout<<" posx: "<<object_1->pos_x<<" posy: "<< object_1->pos_y<<" posz: "<<object_1->pos_z<<" speedx: "<<object_1->speed_x<<" speedy: "<< object_1->speed_y<<" speedz: "<<object_1->speed_z<<"\n";
 }
 
 /* Vector de posicion */
-void vector_position(object *object_1, float time_step)
+void vector_position(object *object_1, double time_step)
 {
     /* Cálculo del vector posición */
     object_1->pos_x = object_1->pos_x + (object_1->speed_x * time_step);
@@ -258,7 +279,7 @@ void vector_position(object *object_1, float time_step)
 }
 
 /* Función para recolocar al objeto si traspasa los límites */
-void check_border(object *object_1, float size_enclosure)
+void check_border(object *object_1, double size_enclosure)
 {
     // Checks posición x 
     if (object_1->pos_x <= 0)
